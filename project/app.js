a3a9707a141958c114c11781972a634ec331dff8
Jeff Chen <jchen42@stuy.edu>
2024-03-02 22:00:26 -0500

    pulling

--- project/app.js
+++ project/app.js
@@ -11,38 +11,38 @@ createApp({
         };
     },
     methods: {
-        calc_result(){
+        calc_result() {
             this.eqn = this.value.split(" ")
             this.eqn_string = ""
             for (var i = 0; i < this.eqn.length; i++) {
-                if(this.eqn[i] == "×") {
+                if (this.eqn[i] == "×") {
                     this.eqn[i] = "*"
-                } 
-                if(this.eqn[i] == "÷"){
+                }
+                if (this.eqn[i] == "÷") {
                     this.eqn[i] = "/"
                 }
-                if(this.eqn[i] == "√") {
-                    if(this.eqn[i+1] == "" && this.eqn[i+2] == "(") {
+                if (this.eqn[i] == "√") {
+                    if (this.eqn[i + 1] == "" && this.eqn[i + 2] == "(") {
                         this.eqn[i] = "Math.sqrt"
                     }
                     else {
                         this.eqn[i] = "Math.sqrt("
-                        this.eqn[i+1] += ")"
+                        this.eqn[i + 1] += ")"
                     }
                 }
-                if(this.eqn[i] == "²") {
+                if (this.eqn[i] == "²") {
                     this.eqn[i] = "** 2"
                 }
-                if(this.eqn[i] == "%"){
+                if (this.eqn[i] == "%") {
                     this.eqn[i] = "/100"
                 }
-                if(this.eqn[i] == "mod"){
+                if (this.eqn[i] == "mod") {
                     this.eqn[i] = "%"
                 }
-                if(this.eqn[i] == "π"){
+                if (this.eqn[i] == "π") {
                     this.eqn[i] = "Math.PI"
                 }
-                if(this.eqn[i].includes("π")){
+                if (this.eqn[i].includes("π")) {
                     this.eqn[i] = this.eqn[i].substring(0, this.eqn[i].length - 1) + "*Math.PI"
                 }
                 this.eqn_string += this.eqn[i]
@@ -50,7 +50,7 @@ createApp({
             this.result = " = " + eval(this.eqn_string)
         },
         backspace() {
-            this.value = this.value.slice(0,-1)
+            this.value = this.value.slice(0, -1)
         },
         add_char(x) {
             this.value += x
